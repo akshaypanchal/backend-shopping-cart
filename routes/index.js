8f2d2d4b3d376c049d3343e954e0445ce6d6c099
unknown <akshaypanchal67@gmail.com>
2021-01-06 18:23:45 -0500

    implement the login functonality

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 
 
 // bcrypt.compare(hash, hash1, function(err, result){
@@ -50,6 +51,7 @@ router.patch('/checkout', function (req, res, next) {
     MongoClient.connect(url, { useUnifiedTopology: true },
       function (err, db) {
         if (err) {
+          // res.status(409).send(err);
           throw err;
         }
         else {
@@ -58,6 +60,8 @@ router.patch('/checkout', function (req, res, next) {
             {
               $inc: { "no_of_items": -1 },
             });
+
+          // res.status(200).send("success");
         }
       });
 
@@ -122,12 +126,12 @@ router.delete('/wishlist', function (req, res, next) {
         throw err;
       }
       else {
-          console.log(req.body);
-          console.log("request header");
-          console.log(req.query);
+        console.log(req.body);
+        console.log("request header");
+        console.log(req.query);
         var dbo = db.db("mydb");
         dbo.collection('wishlist').deleteOne({
-          "_id":req.query._id
+          "_id": req.query._id
         });
       }
 
@@ -142,7 +146,7 @@ router.delete('/wishlist', function (req, res, next) {
 
 router.post('/signup', function (req, res, next) {
 
-    console.log(req.body);
+  console.log(req.body);
 
   MongoClient.connect(url, { useUnifiedTopology: true },
     function (err, db) {
@@ -159,5 +163,67 @@ router.post('/signup', function (req, res, next) {
     });
 
 });
+
+
+router.post("/login", function (req, res, next) {
+
+  let email = req.body.email;
+  let password = req.body.password;
+
+
+
+  MongoClient.connect(url, { useUnifiedTopology: true },
+    function (err, db) {
+      if (err) {
+        throw err;
+      }
+      else {
+        var dbo = db.db("mydb");
+        dbo.collection('users').find({ $and: [{ "email": email }, { "password": password }] }).toArray(function (err, result) {
+          if (err) {
+            throw err;
+          }
+          else {
+            // res.status(200).json({ data: result }););
+            if (result.length > 0) {
+              console.log("function called");
+
+              const token = jwt.sign(
+                // payload
+                { user: email },
+                // sercret key
+                "dreams",
+                // header 
+                {
+                  algorithm: "HS256",
+                  expiresIn: 3600   //seconds
+                }
+              )
+
+              // set it the response's cookie
+              res.cookie('AuthToken', token, { maxAge: 3600 * 1000 });
+              res.redirect('/shopping');
+
+            }
+            else {
+              res.send("Invalid login details");
+            }
+
+            // res.status(200);
+            res.end();
+            db.close();
+          }
+        });
+      }
+    });
+
+
+
+
+
+});
+
+
+
 
 module.exports = router;
